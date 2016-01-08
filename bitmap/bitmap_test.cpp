4028367ec92899f6397f9e0ca7cdb109427f5462
Razvan <stancioiu.razvan@gmail.com>
2016-01-08 11:28:05 +0100

    Add of images for letters

---
+++ bitmap/bitmap_test.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "bitmap_image.hpp"
+#define N 7
+#define M 4
+
+using namespace std;
+
+int main()
+{
+  bitmap_image image("./input.bmp");
+  if(!image)
+  {
+  	printf("Error -failed to open: input.bmp\n");
+  	return 1;
+  }
+
+  unsigned char red,green,blue;
+  const unsigned int height=image.height();
+  const unsigned int width =image.width();
+  int k=0;
+  for(int i=0;i<N;++i)
+  {
+  	for(int j=0;j<M;++j)
+  	{
+  		size_t a = i*width/N;
+  		size_t b = (i+1)*width/N;
+  		size_t c = j*height/M;
+  		size_t d = (j+1)*height/M;
+  		int pos = j*N+i;
+  		if(pos!=21 && pos!=27)
+  		{
+  			string s="";
+  			s+=(char)('A'+k);
+  			s+=".bmp";
+  			const char * filename = s.c_str();
+  			freopen(filename,"w",stdout);
+  			for(size_t y=c;y<d;++y)
+  			{
+  				for(size_t x=a;x<b;++x)
+  				{
+  					image.get_pixel(x,y,red,green,blue);
+  					cout<<(int)red<<" "<<(int)green<<" "<<(int)blue<<endl;
+  				}
+  			}
+  			k++;
+  		}
+  	}
+  }
+  return 0;
+}
+
+
--- bitmap/main.cpp
+++ bitmap/main.cpp
@@ -4,32 +4,53 @@
 #include <string>
 
 #include "bitmap_image.hpp"
+#define N 7
+#define M 4
 
 using namespace std;
 
 int main()
 {
-  freopen("test.out","w",stdout);
-  bitmap_image image("./test.bmp");
+  bitmap_image image("./input.bmp");
   if(!image)
   {
-  	printf("Error -failed to open: input.bmp\n");
-  	return 1;
+    printf("Error -failed to open: input.bmp\n");
+    return 1;
   }
 
   unsigned char red,green,blue;
   const unsigned int height=image.height();
   const unsigned int width =image.width();
-
-  for(size_t y=0;y<height;++y)
+  int k=0;
+  for(int i=0;i<N;++i)
   {
-  	for(size_t x=0;x<width;++x)
-  	{
-  		image.get_pixel(x,y,red,green,blue);
-  		cout<<(int)red<<" "<<(int)green<<" "<<(int)blue<<endl;
-  	}
+    for(int j=0;j<M;++j)
+    {
+      size_t a = i*width/N;
+      size_t b = (i+1)*width/N;
+      size_t c = j*height/M;
+      size_t d = (j+1)*height/M;
+      int pos = j*N+i;
+      if(pos!=21 && pos!=27)
+      {
+        string s="test/";
+        s+=(char)('A'+k);
+        s+=".out";
+        const char * filename = s.c_str();
+        freopen(filename,"w",stdout);
+        for(size_t y=c;y<d;++y)
+        {
+          for(size_t x=a;x<b;++x)
+          {
+            image.get_pixel(x,y,red,green,blue);
+            cout<<(int)red<<" "<<(int)green<<" "<<(int)blue<<endl;
+          }
+        }
+        k++;
+      }
+    }
   }
-   return 0;
+  return 0;
 }
 
 
